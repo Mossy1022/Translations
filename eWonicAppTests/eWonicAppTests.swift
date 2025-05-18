aed15445e59b971040c56087806414f499398777
mossy1022 <evan.x.moscoso@gmail.com>
2025-05-18 17:14:42 -0400

    Initial Commit

---
+++ eWonicAppTests/eWonicAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  eWonicAppTests.swift
+//  eWonicAppTests
+//
+//  Created by Evan Moscoso on 5/18/25.
+//
+
+import Testing
+@testable import eWonicApp
+
+struct eWonicAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
